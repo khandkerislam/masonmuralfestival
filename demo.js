edd20fc4250c7df144b8d2d1908167fa8d6106be
Khandker Islam <khandker.islam46@gmail.com>
2018-06-24 19:33:25 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,12 +1,4 @@
-function initMap() {
-      // The location of Uluru
-      var loc = {lat: 38.831026, lng: -77.307171};
-      // The map, centered at Uluru
-      var map = new google.maps.Map(
-          document.getElementById('map'), {zoom: 18, center: loc});
-      // The marker, positioned at Uluru
-      var marker = new google.maps.Marker({position: loc, map: map});
-    }
+
 (function() {
 
     // Declaring variables
